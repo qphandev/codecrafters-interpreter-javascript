e69db9d2f365d314203b845c49af5ac0faac65e6
qphandev <quynhphandev@gmail.com>
2024-12-10 22:15:32 -0500

    Scanning: Assignment & equality Operators #mp7

--- app/main.js
+++ app/main.js
@@ -23,61 +23,141 @@ const filename = args[1];
 
 const fileContent = fs.readFileSync(filename, "utf8");
 
-if (fileContent.length !== 0) {
+const TokenType = {
+  // Single-character tokens.
+  LEFT_PAREN: { name: "LEFT_PAREN", lexeme: "(" },
+  RIGHT_PAREN: { name: "RIGHT_PAREN", lexeme: ")" },
+  LEFT_BRACE: { name: "LEFT_BRACE", lexeme: "{" },
+  RIGHT_BRACE: { name: "RIGHT_BRACE", lexeme: "}" },
+  COMMA: { name: "COMMA", lexeme: "," },
+  DOT: { name: "DOT", lexeme: "." },
+  MINUS: { name: "MINUS", lexeme: "-" },
+  PLUS: { name: "PLUS", lexeme: "+" },
+  SEMICOLON: { name: "SEMICOLON", lexeme: ";" },
+  SLASH: { name: "SLASH", lexeme: "/" },
+  STAR: { name: "STAR", lexeme: "*" },
 
+  // One or two character tokens.
+  BANG: { name: "BANG", lexeme: "!" },
+  BANG_EQUAL: { name: "BANG_EQUAL", lexeme: "!=" },
+  EQUAL: { name: "EQUAL", lexeme: "=" },
+  EQUAL_EQUAL: { name: "EQUAL_EQUAL", lexeme: "==" },
+  GREATER: { name: "GREATER", lexeme: ">" },
+  GREATER_EQUAL: { name: "GREATER_EQUAL", lexeme: ">=" },
+  LESS: { name: "LESS", lexeme: "<" },
+  LESS_EQUAL: { name: "LESS_EQUAL", lexeme: "<=" },
+
+  // Literals.
+  IDENTIFIER: { name: "IDENTIFIER", lexeme: "" },
+  STRING: { name: "STRING", lexeme: "" },
+  NUMBER: { name: "NUMBER", lexeme: "" },
+
+  // Keywords.
+  AND: { name: "AND", lexeme: "and" },
+  CLASS: { name: "CLASS", lexeme: "class" },
+  ELSE: { name: "ELSE", lexeme: "else" },
+  FALSE: { name: "FALSE", lexeme: "false" },
+  FUN: { name: "FUN", lexeme: "fun" },
+  FOR: { name: "FOR", lexeme: "for" },
+  IF: { name: "IF", lexeme: "if" },
+  NIL: { name: "NIL", lexeme: "nil" },
+  OR: { name: "OR", lexeme: "or" },
+  PRINT: { name: "PRINT", lexeme: "print" },
+  RETURN: { name: "RETURN", lexeme: "return" },
+  SUPER: { name: "SUPER", lexeme: "super" },
+  THIS: { name: "THIS", lexeme: "this" },
+  TRUE: { name: "TRUE", lexeme: "true" },
+  VAR: { name: "VAR", lexeme: "var" },
+  WHILE: { name: "WHILE", lexeme: "while" },
+
+  EOF: { name: "EOF", lexeme: "" }
+};
+
+const tokens = [];
+let start = 0;
+let current = 0;
+let line = 1;
+
+function scanToken(character) {
   // Exit 65
   let isError65 = false;
 
-  let lineCounter = 1;
-  // break apart 
-  let i = 0;
-  while (i < fileContent.length) {
-    const c = fileContent[i];
-    switch (c) {
-      case '(':
-        console.log('LEFT_PAREN ( null');
-        break;
-      case ')':
-        console.log('RIGHT_PAREN ) null');
-        break;
-      case '{':
-        console.log('LEFT_BRACE { null');
-        break;
-      case '}':
-        console.log('RIGHT_BRACE } null');
-        break;
-      case '*':
-        console.log('STAR * null');
-        break;
-      case '.':
-        console.log('DOT . null');
-        break;
-      case ',':
-        console.log('COMMA , null');
-        break;
-      case '+':
-        console.log('PLUS + null');
-        break;
-      case '-':
-        console.log('MINUS - null');
-        break;
-      case ';':
-        console.log('SEMICOLON ; null');
-        break;
-      case '\n':
-        ++lineCounter;
-        break;
-      default:
-        console.error(`[line ${lineCounter}] Error: Unexpected character: ${c}`);
-        isError65 = true;
-    }
-    ++i;
+  switch (character) {
+    case '(':
+      console.log('LEFT_PAREN ( null');
+      break;
+    case ')':
+      console.log('RIGHT_PAREN ) null');
+      break;
+    case '{':
+      console.log('LEFT_BRACE { null');
+      break;
+    case '}':
+      console.log('RIGHT_BRACE } null');
+      break;
+    case '*':
+      console.log('STAR * null');
+      break;
+    case '.':
+      console.log('DOT . null');
+      break;
+    case ',':
+      console.log('COMMA , null');
+      break;
+    case '+':
+      console.log('PLUS + null');
+      break;
+    case '-':
+      console.log('MINUS - null');
+      break;
+    case ';':
+      console.log('SEMICOLON ; null');
+      break;
+    case '=':
+      peekAndMatch('=') ? console.log('EQUAL_EQUAL == null') : console.log('EQUAL = null');
+      break;
+    case '\n':
+      ++line;
+      break;
+    default:
+      console.error(`[line ${line}] Error: Unexpected character: ${c}`);
+      isError65 = true;
   }
-  console.log("EOF  null");
+
+  tokens.push(TokenType.EOF);
 
   if (isError65) {
     process.exit(65);
   }
+}
+
+// HELPERS
+function isAtEnd() {
+  return current >= fileContent.length;
+}
+
+function advance() {
+  return fileContent[current++];
+}
+
+function peekAndMatch(expected) {
+  if (isAtEnd()) return false;
+  if (fileContent[current] !== expected) return false;
+
+  ++current;
+  return true;
+}
+
+// BEGIN READING FILE
+if (fileContent.length !== 0) {
+
+  while (!isAtEnd()) {
+    start = current;
+    const character = advance();
+    
+    scanToken(character);
+  }
+  console.log("EOF  null");
 }
 else {
   console.log("EOF  null");
